2f9b33951801fae0fd37315c9322bd181228a7d2
caotrongtin99 <tin.caotrong@gmail.com>
2022-10-06 17:35:30 +0700

    people infinity scroll

--- infinite-swapi/src/people/InfinitePeople.jsx
+++ infinite-swapi/src/people/InfinitePeople.jsx
@@ -1,4 +1,5 @@
 import InfiniteScroll from "react-infinite-scroller";
+import { useInfiniteQuery } from "react-query";
 import { Person } from "./Person";
 
 const initialUrl = "https://swapi.dev/api/people/";
@@ -8,6 +9,24 @@ const fetchUrl = async (url) => {
 };
 
 export function InfinitePeople() {
-  // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery(
+    'sw-people',
+    ({ pageParams = initialUrl }) => fetchUrl(pageParams),
+    {
+      getNextPageParam: (lastPage) => lastPage.next
+    }
+  )
+
+  return <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
+    {data && data.pages.map(page => {
+      return page.results.map(person => (
+        <Person
+          key={person.name}
+          name={person.name}
+          hairColor={person.hair_color}
+          eyeColor={person.eye_color}
+        />
+      ))
+    })}
+  </InfiniteScroll>;
 }
